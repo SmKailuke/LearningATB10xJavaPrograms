d42a739c7dcabd61102a931b46b5cea4d02dab93
Sonali <sonali.kailuke28@gmail.com>
2025-01-09 00:24:04 +0530

    3rd

---
+++ src/Lab004_multimethod.java
@@ -0,0 +1,7 @@
+public class Lab004_multimethod {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Software");
+        System.out.println("Tester");
+    }}
+

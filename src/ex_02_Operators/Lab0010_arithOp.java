66980a7e138d37b925b88bfff667c31d105106a0
Sonali <sonali.kailuke28@gmail.com>
2025-01-19 16:06:56 +0530

    19th jan

---
+++ src/ex_02_Operators/Lab0010_arithOp.java
@@ -0,0 +1,19 @@
+package ex_02_Operators;
+
+public class Lab0010_arithOp {
+    public static void main(String[] args) {
+        int a= 10;
+        int b=20;
+        int c= a+b; //add
+        System.out.println(c);
+        int c1= a-b; //sub
+        System.out.println(c1);
+        int c2= a*b; //multi
+        System.out.println(c2);
+        int c3= b/a; //div
+        System.out.println(c3);
+        int c4= b%a; //modulus -remainder
+        System.out.println(c4);
+
+    }
+}
